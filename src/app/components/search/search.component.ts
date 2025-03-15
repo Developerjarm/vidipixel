12ab31592dfdc07b16cb8c02850a1e38eb48ba3b
Atriox <developerjarm116@gmail.com>
2025-03-14 20:04:38 -0500

    correct grid images search peceles

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //services
 import { FetchPexelesService } from 'src/app/services/fetch-pexeles.service';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,14 +9,26 @@ import { FetchPexelesService } from 'src/app/services/fetch-pexeles.service';
 })
 export class SearchComponent {
   
+// data pexeles
 public pexeles: any = [];
+//page comun
 public page: number = 1;
+//page search
 public pageSearch: number = 1;
 public tittleSearch: string = '';
+//data pexeles search
 public pexelesSearch: any = [];
+//visible search
 public visible: boolean = false;
-  constructor(private fetchPexelesService: FetchPexelesService) { }
+//suggestions search
+public suggestions: any = [];
 
+  constructor(private fetchPexelesService: FetchPexelesService) { }
+  ngOnInit(): void {
+    this.fetchPexelesService.getPexeles(1).subscribe(data => {
+      this.pexeles = data.photos;
+    });
+  }
 nextPage(){
 this.page++;
 this.fetchPexelesService.getPexeles(this.page).subscribe(data => {
@@ -36,13 +49,15 @@ backPage(){
     }
   });
 }
-
 searchPhoto(){
 this.fetchPexelesService.searchPexeles(this.tittleSearch,this.pageSearch).subscribe(data => {
   this.pexelesSearch = [];
     this.pexelesSearch = data.photos;
     this.visible = true;
-    
+    const element = document.getElementById('container3');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   });
 }
 backPageSearch(){
@@ -61,17 +76,32 @@ nextPageSearch(){
   this.fetchPexelesService.searchPexeles(this.tittleSearch,this.pageSearch).subscribe(data => {
     this.pexelesSearch = data.photos;
     this.visible = true;
-    const element = document.getElementById('container3');
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
+    this.guiaSearch();
   });
 }
 
-  ngOnInit(): void {
-    this.fetchPexelesService.getPexeles(1).subscribe(data => {
-      this.pexeles = data.photos;
-    });
+searchPhotoSuggestions(){
+  this.fetchPexelesService.getSuggestions(this.tittleSearch).subscribe(data => {
+    debounceTime(300), 
+    distinctUntilChanged()
+    this.suggestions = data.results;
+    console.log(data);
+    
+    // this.visible = true;
+    // const element = document.getElementById('container3');
+    // if (element) {
+    //   element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    // }
+  });
+}
+
+guiaSearch(){
+  const element = document.getElementById('container3');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
+}
+
+
 
 }
--- src/app/services/fetch-pexeles.service.ts
+++ src/app/services/fetch-pexeles.service.ts
@@ -12,9 +12,9 @@ export class FetchPexelesService {
   private readonly apiKey:string = 'q4uuxySUr0OteYwRRj5gv4bLzwfmLqcEykkfmAcaewkH87o03UTz8h6w';
 
   constructor( private http: HttpClient) { }
-
+//search photos
   searchPexeles(query: string,page: number): Observable<any> {
-    const url = `${this.apiUrl}/search?query=${encodeURIComponent(query)}&page=${page}&per_page=100`;
+    const url = `${this.apiUrl}/search?query=${encodeURIComponent(query)}&page=${page}&per_page=80`;
     const headers = new HttpHeaders({
       Authorization: this.apiKey 
     });
@@ -25,7 +25,7 @@ export class FetchPexelesService {
       })
     );
   }
-
+//get photos current
   getPexeles(page: number): Observable<any> {
     const url = `${this.apiUrl}/curated?per_page=100&page=${page}`;
     const headers = new HttpHeaders({
@@ -38,6 +38,13 @@ export class FetchPexelesService {
         return throwError(() => new Error('No se pudo obtener imágenes de Pexels'));
       })
     );
+  }
+
+  getSuggestions(query: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: this.apiKey
+    });
+    return this.http.get(`${this.apiUrl}/search?query=${query}`, { headers });
   }
 
 
